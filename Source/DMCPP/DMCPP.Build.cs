57f06d4f5db3d4d53974d5c3d9f99647feefed4c
Rathh34 <sartoreenzo34@gmail.com>
2026-02-16 11:23:02 +0100

    Initial commit

---
+++ Source/DMCPP/DMCPP.Build.cs
@@ -0,0 +1,44 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class DMCPP : ModuleRules
+{
+	public DMCPP(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"DMCPP",
+			"DMCPP/Variant_Horror",
+			"DMCPP/Variant_Horror/UI",
+			"DMCPP/Variant_Shooter",
+			"DMCPP/Variant_Shooter/AI",
+			"DMCPP/Variant_Shooter/UI",
+			"DMCPP/Variant_Shooter/Weapons"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/DMCPP/DMCPP.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DMCPP.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DMCPP, "DMCPP" );
+
+DEFINE_LOG_CATEGORY(LogDMCPP)
